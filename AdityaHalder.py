b981cdec5a61837868eab08da8c351842e10757e
L2LUCKY <145443484+L2LUCKY@users.noreply.github.com>
2024-11-30 19:10:47 +0530

    Update AdityaHalder.py

--- AdityaHalder.py
+++ AdityaHalder.py
@@ -78,13 +78,13 @@ LOGGER = logging.getLogger("SYSTEM")
 if os.path.exists("Config.env"):
     load_dotenv("Config.env")
 
-API_ID = int(getenv("API_ID", 0))
-API_HASH = getenv("API_HASH", None)
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-STRING_SESSION = getenv("STRING_SESSION", None)
-MONGO_DB_URL = getenv("MONGO_DB_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", 0))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", 0))
+API_ID = int(getenv("API_ID", "25609334"))
+API_HASH = getenv("API_HASH", "ad0ff353206ea1ebce1ab8bfca9f3f7b")
+BOT_TOKEN = getenv("BOT_TOKEN", "7532425557:AAGv6DCIjPoX0xiWMwCs6ybCJBBqE0x1NzY")
+STRING_SESSION = getenv("STRING_SESSION", "")
+MONGO_DB_URL = getenv("MONGO_DB_URL", "")
+OWNER_ID = int(getenv("OWNER_ID", ""))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001947334164"))
 START_IMAGE_URL = getenv("START_IMAGE_URL", None)
 
 
